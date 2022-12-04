e346607fedeb57ae4ce6a03331329fc4ef77896a
jaySmilet <36183760+jaySmilet@users.noreply.github.com>
2022-12-05 00:54:36 +0530

    2 files modified

--- i18n.js
+++ i18n.js
@@ -1,28 +1,19 @@
 import i18n from 'i18next';
 import Backend from 'i18next-xhr-backend';
 import { initReactI18next } from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
 
 i18n
   .use(Backend)
+  .use(LanguageDetector)
   .use(initReactI18next)
   .init({
-    lng: 'en', //default language
-    fallbackLng: 'en', //when specified language translations not present
-    //then fallbacklang translations loaded.
+    fallbackLng: 'en',
     debug: true,
-    backend: {
-      /* translation file path */
 
-      loadPath: 'https://cdn.mindbowser.com/assets/i18n/{{ns}}/{{lng}}.json',
-    },
-    /* can have multiple namespace, in case you want to divide a huge translation into smaller pieces and load them on demand */
-    ns: ['translations'],
-    defaultNS: 'translations',
-    keySeparator: false,
     interpolation: {
-      escapeValue: false,
-      formatSeparator: ',',
-    },
-    react: {},
+      escapeValue: false, // not needed for react as it escapes by default
+    }
   });
+
 export default i18n;
--- index.tsx
+++ index.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import './i18n';
 
 import App from './App';
 
@@ -9,6 +10,8 @@ const root = createRoot(rootElement);
 
 root.render(
   <StrictMode>
-    <App />
+    <React.Suspense fallback="loading">
+      <App />
+    </React.Suspense>
   </StrictMode>
 );
