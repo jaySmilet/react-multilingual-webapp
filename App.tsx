d0396704957f2f274944aa0ba928635edd0a9cbb
jaySmilet <36183760+jaySmilet@users.noreply.github.com>
2022-12-04 23:38:55 +0530

    2 files modified, 2 added, 1 deleted

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
 import * as React from 'react';
+import { useTranslation } from 'react-i18next';
 import './style.css';
 
 export default function App() {
+  const { t, i18n } = useTranslation();
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
+      <h1>{t('intro.label')}</h1>
       <p>Start editing to see some magic happen :)</p>
     </div>
   );
--- i18n.js
+++
@@ -1,3 +0,0 @@
-import i18n from 'i18next';
-import Backend from 'i18next-xhr-backend';
-import { initReactI18next } from 'react-i18next';
