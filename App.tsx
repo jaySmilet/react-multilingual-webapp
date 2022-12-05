442efc7e5b527e5425e4dcd7dcc09f919f62db26
jaySmilet <36183760+jaySmilet@users.noreply.github.com>
2022-12-06 01:40:20 +0530

    bootstrap added

--- App.tsx
+++ App.tsx
@@ -1,14 +1,20 @@
 import * as React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Header from './components/nav/Header';
+
+// Components
 import Home from './pages/Home';
+import Profile from './pages/Profile';
 
 import './style.css';
 
 export default function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
-        <Route exact path="/" component={Home} />
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/profile" element={<Profile />} />
       </Routes>
     </BrowserRouter>
   );
