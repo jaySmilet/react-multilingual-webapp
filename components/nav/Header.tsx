7623c53bd225b09001504c85d9228f5f4257034f
jaySmilet <36183760+jaySmilet@users.noreply.github.com>
2022-12-06 01:57:11 +0530

    1 file modified

--- components/nav/Header.tsx
+++ components/nav/Header.tsx
@@ -1,10 +1,33 @@
 import * as React from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
-    <div>
-      <h3>Header</h3>
-    </div>
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <Link className="navbar-brand" to="/">
+        You are here
+      </Link>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarNav">
+        <ul className="navbar-nav ml-auto">
+          <li className="nav-item ml-2">
+            <Link className="nav-link" to="/profile">
+              Profile
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
   );
 };
 
